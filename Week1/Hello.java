7ba0b9ce8972c3a8245d520bb69a2035ae1c7ff1
vinayakstha <vsth005@gmail.com>
2024-04-11 10:30:05 +0545

    week 1 task 2

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -1,9 +1,17 @@
 package Week1;
 
-// Write a program that prints Hello World! to the console. Save the program as Hello.java.
+//1. Write a program that prints Hello World! to the console. Save the program as Hello.java.
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        /*
+         * 2. Extend/Modify the above program to print two additional lines:
+         * 
+         * This is my first program
+         * 
+         * I am on module ST4003CEM
+         */
         System.out.print("This is my first program\n");
         System.out.print("I am on module ST4003CEM");
     }
