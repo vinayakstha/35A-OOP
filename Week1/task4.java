d88d9e424438c6322e3cf2d06f27aab899a69b77
vinayakstha <vsth005@gmail.com>
2024-04-11 10:49:54 +0545

    task

---
+++ Week1/task4.java
@@ -0,0 +1,7 @@
+package Week1;
+
+public class task4 {
+    public static void main(String[] args) {
+        System.out.print("*\n**\n***\n****\n");
+    }
+}
---
+++ Week1/task5.java
@@ -0,0 +1,9 @@
+package Week1;
+
+public class task5 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            System.out.println("*****");
+        }
+    }
+}
