23b09b5895a8bb712373d13345fa3f4d7a1f3bb3
vinayakstha <vsth005@gmail.com>
2024-04-11 10:24:54 +0545

    updated

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -5,6 +5,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.print("This is my first program\n");
-        System.out.print("I am on ST4003CEM");
+        System.out.print("I am on module ST4003CEM");
     }
 }
