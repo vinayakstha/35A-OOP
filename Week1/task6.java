82f355a3b832f05ef0c764e48bde50ba2d3c2190
vinayakstha <vsth005@gmail.com>
2024-04-11 10:52:40 +0545

    task6

---
+++ Week1/task6.java
@@ -0,0 +1,8 @@
+package Week1;
+
+public class task6 {
+    public static void main(String[] args) {
+        System.out.println(
+                "A well-formed Java program has a main method with { and } braces.\nA System.out.println statement has ( and ) and usually a String that starts and ends with a \" character.\n(But we type \" instead!)\nWhat is the difference between\na ' and a \"?  Or between a \" and a \"?\nOne is what we see when we're typing our program. The other is what appears on the \"console.\"");
+    }
+}
