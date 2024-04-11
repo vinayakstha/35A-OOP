bd902c61a90382f5fe52ce9b6a55d52b43db9894
vinayakstha <vsth005@gmail.com>
2024-04-11 10:11:29 +0545

    Week 1 started

---
+++ Week1/Hello.java
@@ -0,0 +1,7 @@
+package Week1;
+// Write a program that prints Hello World! to the console. Save the program as Hello.java.
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
