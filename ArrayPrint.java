fe6c8a6552cb28c8d6ad38cf5bf2274ec87878de
vinayakstha <vsth005@gmail.com>
2024-04-17 20:52:50 +0545

    array

---
+++ ArrayPrint.java
@@ -0,0 +1,16 @@
+public class ArrayPrint {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        for (int i = 0; i < arr.length; i++) {
+            if (i == 0) {
+                System.out.print("[");
+            }
+            System.out.print(arr[i]);
+            if (i != arr.length - 1) {
+                System.out.print(",");
+            } else if (i == arr.length - 1) {
+                System.out.print("]");
+            }
+        }
+    }
+}
