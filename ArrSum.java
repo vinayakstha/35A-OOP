75c74db8e073c894b610500d2e56b2b10ff31e30
vinayakstha <vsth005@gmail.com>
2024-04-17 18:22:20 +0545

    sum of array

---
+++ ArrSum.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ArrSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter length of the array: ");
+        int arraySize = scanner.nextInt();
+
+        // creating an array
+        int[] arr = new int[arraySize];
+        for (int i = 0; i < arraySize; i++) {
+            System.out.print("Enter array elements: ");
+            int arrayElement = scanner.nextInt();
+            arr[i] = arrayElement;
+        }
+
+        // sum of array
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.println("the sum of the array is: " + sum);
+        scanner.close();
+    }
+}
