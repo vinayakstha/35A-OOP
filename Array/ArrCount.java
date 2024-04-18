c8c0e91ae1256e22cea1843ffc3990f660ecbc9e
vinayakstha <vsth005@gmail.com>
2024-04-18 15:27:01 +0545

    array

---
+++ Array/ArrCount.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class ArrCount {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int even = 0;
+        int odd = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+        System.out.println("the no. of even element is " + even);
+        System.out.println("the no. of odd element is " + odd);
+    }
+}
